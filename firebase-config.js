8aba93d01b8328b29ad8d79f0ba9cae256fb243f
ibr-89 <ialhothail@gmail.com>
2026-09-10 21:03:33 +0300

    Update Firebase API key and app ID

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyAYXepGT-er3s8XvPsFEvuOuwyW05ZPX4",
+  apiKey: "AIzaSyAYXepGI-_er3s8XyPsFFyuOuwvW05ZPX4",
   authDomain: "fahad-math-battle.firebaseapp.com",
   databaseURL: "https://fahad-math-battle-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "fahad-math-battle",
   storageBucket: "fahad-math-battle.firebasestorage.app",
   messagingSenderId: "208111433981",
-  appId: "1:208111433981:web:87293c97a2927e5f812694"
+  appId: "1:208111433981:web:e5dc5098bc219081812694"
 };
