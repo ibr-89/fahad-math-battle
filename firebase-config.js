93a0ceed986b3c7ac37c043d15188f4888fba6c9
ibr-89 <ialhothail@gmail.com>
2026-09-11 12:59:55 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAYXepGI-_er3s8XyPsFFyuOuwvW05ZPX4",
+  authDomain: "fahad-math-battle.firebaseapp.com",
+  databaseURL: "https://fahad-math-battle-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "fahad-math-battle",
+  storageBucket: "fahad-math-battle.firebasestorage.app",
+  messagingSenderId: "208111433981",
+  appId: "1:208111433981:web:e5dc5098bc219081812694"
+};
